84153ebb01c50dab1a34514d01cf151e7b9572a6
Gabriellji <gabriellji@yahoo.com>
2020-11-29 18:27:53 +0300

    feat: implement get random fact from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const [number, setNumberFact] = useState('');
+
+  const getNumberFact = (number) => {
+    axios
+    .get(`http://numbersapi.com/${number}/math`)
+    .then(res => {
+      setNumberFact(res.data)
+    })
+    .catch(error => console.log(error));
+  }
+
+  const getRandomNumberFact = () => {
+    axios
+    .get('http://numbersapi.com/random/math')
+    .then(res => {
+      setNumberFact(res.data)
+    })
+    .catch(error => console.log(error));
+  }
   return (
     <div className="App">
       <h1>Hello</h1>
+      {
+        number && <h1>{number}</h1>
+      }
+      <button
+      onClick={getRandomNumberFact}
+      >click me</button>
     </div>
   );
 }
